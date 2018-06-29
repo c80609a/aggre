0840a28f6654361e3dcb8f366378a4a57cb892b1
C80609A <c080609a@gmail.com>
2018-06-29 17:35:14 +0300

    lib/can_pager.rb: поправил баг, добавил пример использования

--- lib/can_pager.rb
+++ lib/can_pager.rb
@@ -9,14 +9,38 @@ end
 
 # usage:
 #
-# ptr   = 1
-# limit = 3
-# arr   = [...]
+# class TestCanPager
+#   include CanPager
 #
-# while true
-#   arr_page = page arr, ptr, limit
-#   puts arr_page
-#   break if arr_page.empty?
-#   ptr += 1
-#   puts '-------'
+#   def initialize
+#     ptr   = 1
+#     limit = 3
+#     arr   = (1..11).to_a.map { (rand*99).to_i }.sort
+#
+#     puts 'limit = %s, arr = [%s] (arr.size = %s)' % [limit, arr * ',', arr.size]
+#     while true
+#       arr_page = page arr, ptr, limit
+#       break if arr_page.nil? || arr_page.empty?
+#       puts '---------------------< page %s ' % ptr
+#       p arr_page
+#       ptr += 1
+#     end
+#
+#   end
+#
 # end
+#
+# TestCanPager.new
+#
+# :: example output::
+#
+# limit = 3, arr = [5,6,14,40,47,52,55,66,69,72,74] (arr.size = 11)
+# ---------------------< page 1
+# [5, 6, 14]
+# ---------------------< page 2
+# [40, 47, 52]
+# ---------------------< page 3
+# [55, 66, 69]
+# ---------------------< page 4
+# [72, 74]
+#
